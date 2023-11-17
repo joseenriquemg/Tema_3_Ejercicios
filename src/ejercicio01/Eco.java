60cd947addefe307601ad9a7dbc12b0a24e1e489
joseenriquemg <josee.munoz@iesnervion.es>
2023-11-17 14:42:32 +0100

    AAAA

---
+++ src/ejercicio01/Eco.java
@@ -0,0 +1,14 @@
+package ejercicio01;
+
+public class Eco {
+
+	public static void eco(int numero) {
+		//Creamos un for para mostrar el numero n veces.
+		for (int i = 1; i < numero; i++) {
+			
+		}
+		
+
+	}
+
+}
